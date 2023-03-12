f2aecf8d16e91e20629627119342ade2fb3d08b3
Nikich1711 <nikolai.vd@abv.bg>
2023-03-12 12:58:04 +0200

    Add project files.

---
+++ Shop.Web/Models/Account/AccountIndexModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Shop.Web.Models.Account
+{
+	public class AccountIndexModel
+	{
+		public IEnumerable<AccountProfileModel> Accounts { get; set; }
+		public string SearchQuery { get; set; }
+	}
+}
---
+++ Site.Data/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace website.Site.Data.Models
+{
+    public class category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public IEnumerable<Images> FoodProducts { get; set; }
+    }
+}
